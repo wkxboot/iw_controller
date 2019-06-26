c7f60ae5b031e8a1da07b4b462185077c6e14b3b
wkxboot <1131204425@qq.com>
2019-06-26 10:00:41 +0800

    压缩机工作2小时停机5分钟

--- iw_controller/board/user/tasks/compressor_task.h
+++ iw_controller/board/user/tasks/compressor_task.h
@@ -20,8 +20,8 @@ extern osMessageQId compressor_task_msg_q_id;
 void compressor_task(void const *argument);
 
 
-#define  COMPRESSOR_TASK_WORK_TIMEOUT                 (270*60*1000) /*连续工作时间单位:ms*/
-#define  COMPRESSOR_TASK_REST_TIMEOUT                 (10*60*1000)  /*连续工作时间后的休息时间单位:ms*/
+#define  COMPRESSOR_TASK_WORK_TIMEOUT                 (120*60*1000) /*连续工作时间单位:ms*/
+#define  COMPRESSOR_TASK_REST_TIMEOUT                 (5*60*1000)   /*连续工作时间后的休息时间单位:ms*/
 #define  COMPRESSOR_TASK_WAIT_TIMEOUT                 (5*60*1000)   /*2次开机的等待时间 单位:ms*/
 
 #define  COMPRESSOR_TASK_PUT_MSG_TIMEOUT               5            /*发送消息超时时间 单位:ms*/
